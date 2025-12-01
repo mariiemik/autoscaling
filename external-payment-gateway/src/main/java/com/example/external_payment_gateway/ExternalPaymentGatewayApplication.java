05bcddcdab0df6bbca47b16b5a6152b7d9709185
Maria Mikhailova <mariiemik@gmail.com>
2025-12-01 19:46:59 +0300

    basic project structure using Docker

---
+++ external-payment-gateway/src/main/java/com/example/external_payment_gateway/ExternalPaymentGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.external_payment_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalPaymentGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalPaymentGatewayApplication.class, args);
+	}
+
+}
---
+++ external-payment-gateway/src/test/java/com/example/external_payment_gateway/ExternalPaymentGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.external_payment_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalPaymentGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
